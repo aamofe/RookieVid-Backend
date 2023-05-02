9082d43704563288566b4f07b92a1c08eb31a888
iwacat <hanxy2001@163.com>
2023-05-02 12:40:03 +0800

    修改注册

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('sendvcode', send_mail_vcode),
-    path('validatevacode', validate_mail_vcode),
+    # path('validatevacode', validate_mail_vcode),
     path('register', register),
     path('login', login),
     path('logout', logout),
--- accounts/views.py
+++ accounts/views.py
@@ -42,44 +42,40 @@ def send_mail_vcode(request):
         request.session['mail_code_time'] = time.time()
     return JsonResponse(resp)
 
-# 验证码是否正确
-@csrf_exempt
-def validate_mail_vcode(request):
-    # 获取输入的验证码和邮箱
-    email = request.POST.get('email')
-    vcode = request.POST.get('vcode')
-    # 从session中获取邮箱和验证码
-    session_email = request.session.get('mail')
-    session_code = request.session.get('mail_code')
 
-    # 判断发送用户是否一致
-    if session_email and session_email == email:
-        # 判断验证码是否失效
-        now_time = time.time()
-        # 获取发送验证码时间
-        session_code_time = request.session.get('mail_code_time')
-        if session_code_time and now_time <= session_code_time + 300:
-            # 验证验证码输入
-            if session_code and session_code == vcode:
-                resp = {'msg': '验证码正确'}
-            else:
-                return JsonResponse({'errno': 1007, 'msg': '验证码错误'})
-        else:
-            return JsonResponse({'errno': 1008, 'msg': '验证码失效，请重新获取'})
-    else:
-        return JsonResponse({'errno': 1009, 'msg': '该账户没有获取验证码'})
-
-    return JsonResponse(resp)
-
+# 先验证验证码是否正确，若正确检验用户名密码是否合法，完成注册
 @csrf_exempt
 def register(request):
     if request.method == 'POST':  # 判断请求方式是否为 POST（要求POST方式）
+        # 获取输入的验证码和邮箱
+        email = request.POST.get('email')
+        vcode = request.POST.get('vcode')
+        # 从session中获取邮箱和验证码
+        session_email = request.session.get('mail')
+        session_code = request.session.get('mail_code')
+        # 判断发送用户是否一致
+        if session_email and session_email == email:
+            # 判断验证码是否失效
+            now_time = time.time()
+            # 获取发送验证码时间
+            session_code_time = request.session.get('mail_code_time')
+            if session_code_time and now_time <= session_code_time + 300:
+                # 验证验证码输入
+                if session_code and session_code == vcode:
+                    resp = {'msg': '验证码正确'}
+                else:
+                    return JsonResponse({'errno': 1007, 'msg': '验证码错误'})
+            else:
+                return JsonResponse({'errno': 1008, 'msg': '验证码失效，请重新获取'})
+        else:
+            return JsonResponse({'errno': 1009, 'msg': '该账户没有获取验证码'})
+        # 验证码正确，进行注册
         username = request.POST.get('username')  # 获取请求数据
         password_1 = request.POST.get('password_1')
         password_2 = request.POST.get('password_2')
-        email = request.POST.get('email')
-        #print("username : ",username)
-
+        # 通过邮箱判断用户是否已存在
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'errno': 1010, 'msg': "账号已存在，请勿重复注册"})
         # 用户名长度为1-20位
         if re.match('.{1,20}', str(username)) is None:
             return JsonResponse({'errno': 1001, 'msg': "用户名不合法"})
@@ -96,7 +92,7 @@ def register(request):
             # 数据库存取：新建 User 对象，赋值并保存
             new_user = User(uid=uid, username=username, password=password_1, email=email)
             new_user.save()  # 一定要save才能保存到数据库中
-            return JsonResponse({'errno': 0, 'msg': "注册成功"})
+            return JsonResponse({'uid': uid, 'errno': 0, 'msg': "注册成功"})
     else:
         return JsonResponse({'error': 1, 'msg': "请求方式错误"})
         # return render(request, 'register.html', {})
@@ -107,10 +103,11 @@ def login(request):
     if request.method == 'POST':
         uid = request.POST.get('uid')  # 获取请求数据
         password = request.POST.get('password')
-        if re.match('[0-9]{10}', str(uid)):
-            user = User.objects.get(id=uid)
-        elif re.match('\w+@\w+.\w+', str(uid)):
-            user = User.objects.get(email=uid)
+        if User.objects.filter(uid=uid).exists() or User.objects.filter(email=uid):
+            if re.match('[0-9]{10}', str(uid)):
+                user = User.objects.get(id=uid)
+            elif re.match('\w+@\w+.\w+', str(uid)):
+                user = User.objects.get(email=uid)
         else:
             return JsonResponse({'errno': 1001, 'msg': "请先注册"})
         if user.password == password:  # 判断请求的密码是否与数据库存储的密码相同
