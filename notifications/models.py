832032a29ed6693fd7f8de8ae7dddb76d34d8b5b
iwacat <hanxy2001@163.com>
2023-05-18 21:08:37 +0800

    发送通知0.0

--- notifications/models.py
+++ notifications/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Notification(models.Model):
-    user_id = models.IntegerField('通知对象ID')
+    send_to = models.CharField('通知对象ID', max_length=10)
+    send_from = models.CharField('通知者', max_length=10)
     content = models.CharField('通知内容', max_length=255)
-    link=models.CharField('跳转链接',max_length=255)
+    # link=models.CharField('跳转链接',max_length=255)
     created_at = models.DateTimeField('通知创建时间', auto_now_add=True)
--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from notifications.models import Notification
+from accounts.models import User
 
 # Create your views here.
+
+# 管理端给用户发送通知
+def send_notification(request):
+    if request.method == 'POST':
+        send_to = request.POST.get("send_to")
+        content = request.POST.get("content")
+        send_from = request.user.uid
+        new_notification = Notification(send_from=send_from, send_to=send_to, content=content)
+        new_notification.save()
+        return JsonResponse({'errno': 0, 'msg': "通知发送成功"})
+    else:
+        return JsonResponse({'error': 1, 'msg': "请求方式错误"})
+
+def send_sys_notification(send_to, send_from, content):
+    if User.objects.filter(uid=send_to).exists():
+        new_notification = Notification(send_from=send_from, send_to=send_to, content=content)
+        new_notification.save()
+        return True
+    else:
+        return False
+
+def check_notification(request):
+    if request.method == 'GET':
+
+        return JsonResponse({'errno': 0, 'msg': "通知发送成功"})
+    else:
+        return JsonResponse({'error': 1, 'msg': "请求方式错误"})
